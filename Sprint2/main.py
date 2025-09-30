7ab189b9be37f14580834fd6b9725dbcafb5f388
MarshallKnisal <Mskgmf@umsystem.edu>
2025-09-30 10:52:11 -0500

    "Sprint 2 9/30/25 10:52AM"

--- Sprint2/main.py
+++ Sprint2/main.py
@@ -1,1 +1,14 @@
-"""Sprint 2 coming soon"""
+import tkinter as tk
+import pylint
+import unittest
+
+
+class GameSetup:
+    def __init__(self,root):
+        self.root.title("Game Setup")
+        self.root.geometry("1000x800)")
+        self.gameplay_mode = tk.StringVar(value="")
+        self.board_size = tk.StringVar()
+        self.create_widgets()
+
+
