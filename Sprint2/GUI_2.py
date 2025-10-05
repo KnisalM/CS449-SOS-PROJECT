8c134d2434afaaaa46fdf210a1a67d0cce9c3ec5
MarshallKnisal <Mskgmf@umsystem.edu>
2025-10-05 13:57:05 -0500

    "Sprint 2 10/5/25 1:56PM"

--- Sprint2/GUI_2.py
+++ Sprint2/GUI_2.py
@@ -3,12 +3,17 @@ import pylint
 import unittest
 
 
-class GameSetup:
-    def __init__(self, root):
-        self.root.title("Game Setup")  # Title of window
-        self.root.geometry("1000x800)")  # Size of window
-        self.gameplay_mode = tk.StringVar(value="")  # Variable to store the selected gameplay mode
-        self.board_size = tk.StringVar()  # Variable to store the board size input by the player, which will be converted to an INT
-        self.create_widgets()  # This function will create and place the widgets in the beginning screen
+class gameBoard():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.rootTitle("SOS Game")
+        self.dimensions = '' # The variable that will store the player's selection for the dimensions of the board
+        self.ruleSet = '' # This variable will store whether the player has selected to play a
+        #self.opponentType = '' Will Implement later, this will store the player's selection for Human v Human or Human v Machine
+        self.currentTurn = ''
+        #self.active = '' Implement in later sprint, will track game state and update when game is over
+        self.cells = []
+        #self.cellsState = [] Implement in later Sprint, will track the state of the cells
+        #self.player1Score = 0
+        #self.player2Score = 0
 
-    def create_widgets(self):
